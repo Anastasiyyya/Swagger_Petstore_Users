4a0557d3ec1328cb59d062e32cfdf2c6acd81690
AnastasiyaShevtsova <anastasiya.shevtsova.jazzteam@gmail.com>
2021-10-05 18:00:36 +0300

    Added new project

---
+++ src/main/java/objects/ApiResponse.java
@@ -0,0 +1,13 @@
+package objects;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ApiResponse {
+
+    private int code;
+    private String type;
+    private String message;
+}
