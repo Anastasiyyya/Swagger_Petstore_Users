b9a7b806d3534693d43e0120800f54b024d48931
AnastasiyaShevtsova <anastasiya.shevtsova.jazzteam@gmail.com>
2021-10-07 17:55:33 +0300

    added some files

--- src/main/java/specs/Specifications.java
+++ src/main/java/specs/Specifications.java
@@ -1,23 +1,27 @@
 package specs;
 
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
+import objects.User;
 
-import java.nio.charset.StandardCharsets;
-
-import static io.restassured.config.EncoderConfig.encoderConfig;
-import static io.restassured.config.RestAssuredConfig.newConfig;
-import static org.hamcrest.Matchers.containsString;
+import static io.restassured.RestAssured.given;
 
 public class Specifications {
 
-    public static RequestSpecification requestSpec = new RequestSpecBuilder()
-            //.setConfig(newConfig().encoderConfig(encoderConfig().defaultContentCharset(StandardCharsets.UTF_8)))
+    public static final String BASE_URL = "https://petstore.swagger.io/v2/";
+
+    public static RequestSpecification SPEC = new RequestSpecBuilder()
+            .setBaseUri(BASE_URL)
             .setContentType(ContentType.JSON)
             .setAccept(ContentType.JSON)
-
             .build();
+
+    public ValidatableResponse createUser(User user) {
+        return given().spec(SPEC)
+                .when().body(user)
+                .post(BASE_URL + "user")
+                .then().log().all();
+    }
 }
