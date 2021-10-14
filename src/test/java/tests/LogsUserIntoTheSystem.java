f97ecdea393b0380ccc376ce71de7554834ded48
AnastasiyaShevtsova <anastasiya.shevtsova.jazzteam@gmail.com>
2021-10-14 11:02:31 +0300

    added some files

--- src/test/java/tests/LogsUserIntoTheSystem.java
+++ src/test/java/tests/LogsUserIntoTheSystem.java
@@ -9,10 +9,102 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class LogsUserIntoTheSystem extends BaseTest {
 
-    @Test(description = "Checks status code and value of 'type' and 'message' keys")
+    @Test(description = "")
     public void logInWithCorrectUsernamePasswordTest() {
 
+        specifications.logout()
+                .body("type", equalTo("unknown"),
+                        "message", containsString("ok"))
+                .statusCode(200);
         specifications.logUserIntoTheSystem(Users.USER2.getUsername(),Users.USER2.getPassword())
+                .body("type", equalTo("unknown"),
+                        "message", containsString("logged in user session"))
+                .statusCode(200);
+
+        specifications.createUser(Users.USER3)
+                .body("type", equalTo("unknown"),
+                        "message", containsString(String.valueOf(Users.USER3.getUserStatus())))
+                .statusCode(200);
+
+        User createdUser = specifications.getUserByUsername(Users.USER3.getUsername()).extract().body().as(User.class);
+
+        Assert.assertEquals(createdUser, Users.USER3);
+    }
+
+    @Test(description = "")
+    public void logInWithIncorrectPasswordTest() {
+        specifications.logout()
+                .body("type", equalTo("unknown"),
+                        "message", containsString("ok"))
+                .statusCode(200);
+
+        specifications.logUserIntoTheSystem(Users.USER2.getUsername(),"IncorrectPSW")
+                .body("type", equalTo("unknown"),
+                        "message", containsString("logged in user session"))
+                .statusCode(200);
+
+        specifications.createUser(Users.USER3)
+                .body("type", equalTo("unknown"),
+                        "message", containsString(String.valueOf(Users.USER3.getUserStatus())))
+                .statusCode(200);
+
+        User createdUser = specifications.getUserByUsername(Users.USER3.getUsername()).extract().body().as(User.class);
+
+        Assert.assertEquals(createdUser, Users.USER3);
+    }
+
+    @Test(description = "")
+    public void logInWithEmptyPasswordFieldTest() {
+        specifications.logout()
+                .body("type", equalTo("unknown"),
+                        "message", containsString("ok"))
+                .statusCode(200);
+
+        specifications.logUserIntoTheSystem(Users.USER2.getUsername(),"")
+                .body("type", equalTo("unknown"),
+                        "message", containsString("logged in user session"))
+                .statusCode(200);
+
+        specifications.createUser(Users.USER3)
+                .body("type", equalTo("unknown"),
+                        "message", containsString(String.valueOf(Users.USER3.getUserStatus())))
+                .statusCode(200);
+
+        User createdUser = specifications.getUserByUsername(Users.USER3.getUsername()).extract().body().as(User.class);
+
+        Assert.assertEquals(createdUser, Users.USER3);
+    }
+
+    @Test(description = "")
+    public void logInWithEmptyUsernameFieldTest() {
+        specifications.logout()
+                .body("type", equalTo("unknown"),
+                        "message", containsString("ok"))
+                .statusCode(200);
+
+        specifications.logUserIntoTheSystem("",Users.USER2.getPassword())
+                .body("type", equalTo("unknown"),
+                        "message", containsString("logged in user session"))
+                .statusCode(200);
+
+        specifications.createUser(Users.USER3)
+                .body("type", equalTo("unknown"),
+                        "message", containsString(String.valueOf(Users.USER3.getUserStatus())))
+                .statusCode(200);
+
+        User createdUser = specifications.getUserByUsername(Users.USER3.getUsername()).extract().body().as(User.class);
+
+        Assert.assertEquals(createdUser, Users.USER3);
+    }
+
+    @Test(description = "")
+    public void logInWithEmptyFieldsTest() {
+        specifications.logout()
+                .body("type", equalTo("unknown"),
+                        "message", containsString("ok"))
+                .statusCode(200);
+
+        specifications.logUserIntoTheSystem("","")
                 .body("type", equalTo("unknown"),
                         "message", containsString("logged in user session"))
                 .statusCode(200);
