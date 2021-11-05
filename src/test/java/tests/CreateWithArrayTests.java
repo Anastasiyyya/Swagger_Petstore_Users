a11780f75ba1889a8a1303b6cc2a171ad737562f
AnastasiyaShevtsova <anastasiya.shevtsova.jazzteam@gmail.com>
2021-11-05 12:54:39 +0300

    fix

--- src/test/java/tests/CreateWithArrayTests.java
+++ src/test/java/tests/CreateWithArrayTests.java
@@ -1,13 +1,9 @@
 package tests;
 
 import Objects.Users;
-import data.UsersList;
 import objects.User;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.hamcrest.Matchers.equalTo;
 
 public class CreateWithArrayTests extends BaseTest{
@@ -22,10 +18,9 @@ public class CreateWithArrayTests extends BaseTest{
                         "message", equalTo("ok"))
                 .statusCode(200);
 
-        List<User> user1 = specifications.getUserByUsername(Arrays.stream(Users.USERS_ARRAY_WITH_TWO_USERS)
-                .findFirst().get().getUsername()).extract().body().jsonPath().getList(".", User.class);
+        User user1 = specifications.getUserByUsername(Users.USERS_LIST_WITH_TWO_USERS.get(0).getUsername()).extract().body().as(User.class);
 
-        Assert.assertEquals(Arrays.stream(Users.USERS_ARRAY_WITH_TWO_USERS).findFirst(), user1.stream().findFirst());
+        Assert.assertEquals(user1, Users.USERS_LIST_WITH_TWO_USERS.get(0));
     }
 
     @Test
